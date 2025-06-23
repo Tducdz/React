cf2156f512c2fccf934475d4dba83e575e193286
Dao Trung Duc <dtducdzz@gmail.com>
2025-06-23 23:05:32 +0700

    bai 23

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,12 +1,19 @@
 import React from "react";
 import UserInfor from "./UseInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   // JSX
   render() {
+    const myInfor = ["ab", "c", "c"];
     return (
       <div>
         <UserInfor />
+        <br />
+        <br />
+        <DisplayInfor name="Trung Duc" age="21" />
+        <br />
+        <DisplayInfor name="Duc" age={22} myInfor={myInfor} />
       </div>
     );
   }
