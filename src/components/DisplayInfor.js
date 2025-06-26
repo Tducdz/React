34e7e3daf64f68921720e2f22df1c20c2da806c7
Dao Trung Duc <dtducdzz@gmail.com>
2025-06-26 16:32:16 +0700

    bai 28

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfor.scss";
+import logo from "../logo.svg";
 
 class DisplayInfor extends React.Component {
   state = {
@@ -16,6 +17,7 @@ class DisplayInfor extends React.Component {
     const { listUsers } = this.props;
     return (
       <div className="display-infor-container">
+        <img src={logo} alt="logo-react" />
         <div>
           <span
             onClick={() => {
