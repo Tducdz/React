6e7f543e391c0686135d9a5c305f10a9197416d3
Dao Trung Duc <dtducdzz@gmail.com>
2025-06-30 21:24:10 +0700

    bai 38

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,35 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
-
-// class DisplayInfor extends React.Component {
-//   render() {
-//     const { listUsers } = this.props;
-//     return (
-//       <div className="display-infor-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>My name is {user.name}</div>
-//                   <div>My age is {user.age}</div>
-//                   <hr />
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfor = (props) => {
   const { listUsers } = props;
@@ -39,6 +9,15 @@ const DisplayInfor = (props) => {
   const handleShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser);
   };
+
+  console.log("call me render");
+
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("you deleted all the users");
+    }
+    console.log("call me useEffect");
+  }, [listUsers]);
 
   return (
     <div className="display-infor-container">
