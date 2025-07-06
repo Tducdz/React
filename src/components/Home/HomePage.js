696768a7f7eeabf439b7b3840cc63e8669bbd965
Dao Trung Duc <dtducdzz@gmail.com>
2025-07-06 14:26:48 +0700

    bai 55

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,23 @@
+import videoHomePage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
-  return <div className="homepage-container">Home page component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+      <div className="homepage-content">
+        <div className="main-title">There's a better way to ask</div>
+        <div className="title-2">
+          You don't want to make a boring form. And you audience won't answer
+          one. Create a typeform instead-and make everyone happy.
+        </div>
+        <div className="title-3">
+          <button>Get's started. It's free</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
