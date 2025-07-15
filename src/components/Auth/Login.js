e2a1a82ac50f8ec4b1e7caaf69c487db9edf9b81
Dao Trung Duc <dtducdzz@gmail.com>
2025-07-15 15:38:44 +0700

    bai 70

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,17 +1,32 @@
 import { useState } from "react";
 import "./Login.scss";
+import { useNavigate } from "react-router-dom";
+import { postLogin } from "../../services/apiService";
+import { toast } from "react-toastify";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    alert("me");
+  const handleLogin = async () => {
+    let res = await postLogin(email, password);
+    if (res && res.EC === 0) {
+      toast.success(res.EM);
+      navigate("/");
+    }
+
+    if (res && res.EC !== 0) {
+      toast.error(res.EM);
+    }
   };
 
   return (
     <div className="login-container">
-      <div className="header">Don't have account yet?</div>
+      <div className="header">
+        <span>Don't have account yet?</span>
+        <button>Sign up</button>
+      </div>
       <div className="title col-4 mx-auto">TDucQuiz</div>
       <div className="welcome col-4 mx-auto">Hello, who's this?</div>
       <div className="content-form col-4 mx-auto">
@@ -36,6 +51,12 @@ const Login = (props) => {
           <button className="btn-submit" onClick={() => handleLogin()}>
             Login
           </button>
+          <div className="btn-home">
+            <span onClick={() => navigate("/")}>
+              {" "}
+              &#60;&#60; Return to Homepage
+            </span>
+          </div>
         </div>
       </div>
     </div>
