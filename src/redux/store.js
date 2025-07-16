f072e1904445efb96ff728b5f0c901f7f7b1d995
Dao Trung Duc <dtducdzz@gmail.com>
2025-07-16 18:54:50 +0700

    bai 80

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,10 +8,20 @@
 // export default store;
 
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import rootReducer from "./reducer/rootReducer";
 
-const store = configureStore({
-  reducer: rootReducer,
-});
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+let persistor = persistStore(store);
+
+export { store, persistor };
