65b78f475ed528002d909c994b0b3ba2e14b8a34
Dao Trung Duc <dtducdzz@gmail.com>
2025-07-21 10:39:28 +0700

    bai 86

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,21 +1,67 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import "./DetailQuiz.scss";
+import Question from "./Question";
+import _ from "lodash";
 
 const DetailQuiz = () => {
   const param = useParams();
   const quizId = param.id;
   const location = useLocation();
 
+  const [dataQuiz, setDataQuiz] = useState([]);
+  const [index, setIndex] = useState(0);
+
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId);
+    if (res?.DT?.qa) {
+      const dataWithSelected = res.DT.qa.map((question) => ({
+        ...question,
+        answers: question.answers.map((ans) => ({
+          ...ans,
+          isSelected: false,
+        })),
+      }));
+
+      setDataQuiz(dataWithSelected);
+    }
   };
 
   useEffect(() => {
     fetchQuestions();
   }, [quizId]);
+
+  const handlePrev = () => {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (dataQuiz && dataQuiz.length > index + 1) {
+      setIndex(index + 1);
+    }
+  };
+
+  const handleCheckbox = (answerId, questionId) => {
+    let dataQuizClone = _.cloneDeep(dataQuiz); // Không được thao tác trực tiếp với state nên clone ra
+    let question = dataQuizClone.find((item) => +item.id === +questionId);
+    if (question && question.answers) {
+      question.answers = question.answers.map((item) => {
+        if (+item.id === +answerId) {
+          item.isSelected = !item.isSelected;
+        }
+        return item;
+      });
+    }
+    console.log(question);
+    let index = dataQuizClone.findIndex((item) => +item.id === +questionId);
+    if (index > -1) {
+      dataQuizClone[index] = question;
+      setDataQuiz(dataQuizClone);
+    }
+  };
 
   return (
     <div className="detail-quiz-container container">
@@ -26,20 +72,27 @@ const DetailQuiz = () => {
           {location?.state?.quizDescription}
         </div>
         <hr />
-        <div className="q-body">
+        {/* <div className="q-body">
           <img />
-        </div>
+        </div> */}
         <div className="q-content">
-          <div className="question">question 1: what is your name?</div>
-          <div className="answer">
-            <div className="option">A. dap an 1</div>
-            <div className="option">B. dap an 2</div>
-            <div className="option">C. dap an 3</div>
-          </div>
+          <Question
+            index={index}
+            handleCheckbox={handleCheckbox}
+            data={dataQuiz && dataQuiz.length > 0 ? dataQuiz[index] : []}
+          />
         </div>
+
         <div className="footer">
-          <button className="btn btn-secondary">Prev</button>
-          <button className="btn btn-primary">Next</button>
+          <button className="btn btn-secondary" onClick={() => handlePrev()}>
+            Prev
+          </button>
+          <button className="btn btn-primary" onClick={() => handleNext()}>
+            Next
+          </button>
+          <button className="btn btn-warning" onClick={() => handleNext()}>
+            Finish
+          </button>
         </div>
       </div>
       <div className="right-content">count down</div>
---
+++ src/components/User/Question.js
@@ -0,0 +1,49 @@
+import _ from "lodash";
+
+const Question = (props) => {
+  const { data, index } = props;
+  if (_.isEmpty(data)) {
+    return <></>;
+  }
+
+  const handleCheckbox = (event, answerId, questionId) => {
+    console.log(answerId, questionId);
+    props.handleCheckbox(answerId, questionId);
+  };
+
+  // console.log(data);
+
+  return (
+    <>
+      <div className="q-img">
+        <img src={`data:image/png;base64,${data.imageFile}`} alt="" />
+      </div>
+
+      <div className="question">
+        Question {index + 1}: {data.description}?
+      </div>
+
+      <div className="answer">
+        {data.answers &&
+          data.answers.length &&
+          data.answers.map((a, index) => {
+            return (
+              <div key={a.id} className="option">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    checked={a.isSelected}
+                    onChange={(event) => handleCheckbox(event, a.id, data.id)}
+                  />
+                  <label className="form-check-label">{a.description}</label>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Question;
